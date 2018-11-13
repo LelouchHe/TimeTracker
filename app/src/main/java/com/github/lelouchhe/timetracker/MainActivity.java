0ea26589fc39f9d9979f9dd664aa693ba9b2af63
Lelouch He <lelouchhe@gmail.com>
2018-11-13 10:49:02 -0800

    setup main activity

---
+++ app/src/main/java/com/github/lelouchhe/timetracker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.github.lelouchhe.timetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivCurrentTaskIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        ivCurrentTaskIcon = this.findViewById(R.id.ivCurrentTaskIcon);
+
+        ivCurrentTaskIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "current task icon clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
